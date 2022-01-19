95587b7df2eff2d0b4cdc91ba45644bf244dba56
anfer-code <anfervalera11@gmail.com>
2022-01-19 19:50:16 -0400

    mejorando: copy, diseño y agregando animaciones

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let menu = document.getElementsByClassName("nav__menu")[0];
 
 let icon = document.getElementsByClassName("material-icons")[0]
 
+const list = document.getElementsByClassName("nav__list")[0]
+const a = document.querySelector("a")
+
 function toogleFunction(ev) {
-    const list = document.getElementsByClassName("nav__list")[0]
 
     if(list.classList.contains("hide")) {
         icon.innerText = "close"
@@ -17,4 +19,13 @@ function toogleFunction(ev) {
     }
 }
 
+function removeMenu(ev) {
+    if(ev.target.classList.contains("nav__a")) {
+        list.classList.add("hide");
+        icon.innerText = "menu";
+    }
+}
+
 menu.addEventListener("click", toogleFunction)
+
+list.addEventListener("click", removeMenu)
